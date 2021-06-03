9a5e15e826c2dc55720ae37b3c44446db4a5d4ce
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2021-06-03 14:04:31 +0800

    can call even when activity is dead & phone locked

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ import App from './src/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+AppRegistry.registerHeadlessTask(
+	'RNCallKeepBackgroundMessage',
+	() =>
+		({name, callUUID, handle}) => {
+			// Make your call here
+			console.log(name, callUUID, handle);
+			return Promise.resolve();
+		},
+);
