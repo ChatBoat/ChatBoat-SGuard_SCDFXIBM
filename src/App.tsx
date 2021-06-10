a7a0b5861f54c4e9f751eaf0060a667f10c8cf4a
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2021-06-10 10:39:22 +0800

    will continue regular commits now whoops

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import { View } from 'react-native';
-import { BotProvider } from './BotProvider';
-import Home from './pages/Home';
+import React from 'react'
+import { View } from 'react-native'
+import { BotProvider } from './BotProvider'
+import Home from './pages/Home'
+import Toast from 'react-native-toast-message'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator, Header } from '@react-navigation/stack'
+import Conversation from './pages/Conversation'
+import Medical from './pages/Medical'
+
+const Stack = createStackNavigator()
 
 function App() {
-  return (
-    <View>
-      <BotProvider>
-        <Home/>
-      </BotProvider>
-    </View>
-  );
+	return (
+		<BotProvider>
+			<NavigationContainer>
+				<Stack.Navigator initialRouteName="home">
+					<Stack.Screen name="home" component={Home} />
+					<Stack.Screen name="conversation" component={Conversation} />
+					<Stack.Screen name="medical" component={Medical} />
+				</Stack.Navigator>
+				<Toast ref={(ref) => Toast.setRef(ref)} />
+			</NavigationContainer>
+		</BotProvider>
+	)
 }
 
-export default App;
+export default App
